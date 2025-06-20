549d0f74fb20a1a4e26e1ccda3ec1922a4d9d176
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-06-20 14:17:31 +0300

    commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event){
+    main();
+});
+function main(){
     displayPosts();
-})
-    function displayPosts(){
+    addNewPostListener();
+
+}
+function displayPosts(){
         fetch("http://localhost:3000/posts")
         .then(response=> response.json())
         .then(posts=>{
             posts.forEach(post =>{
-            const ul = document.querySelector("#post-list");
+            const ul = document.querySelector("#post-list ul");
                let li = document.createElement("li");
                li.textContent = post.title;
                
@@ -44,4 +49,30 @@ function handlePostClick(event){
     
         
     });
+}
+
+
+function addNewPostListener(){
+    const form = document.getElementById("new-post-form");
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        const title = document.getElementById("title").value.trim();
+        const content = document.getElementById("content").value.trim();
+        const author = document.getElementById("author").value.trim();
+
+        if(!title || !content || !author) return;
+        
+        const newPost = {title, content, author};
+        
+        appendPostToList(newPost);
+        form.reset();
+    });
+
+    function appendPostToList(post){
+        const ul=document.querySelector("#post-list ul");
+        const li=document.createElement("li");
+        li.textContent = post.title;
+
+        ul.appendChild(li);
+    }
 }
